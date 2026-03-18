7a7fb0fc4fde4dcfa2005a3050c17e963db9f8aa
TanyaA318 <tata4625@gmail.com>
2026-03-18 17:48:31 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./styles.css";
 import Badboy from "./Badboy";
 const navItems = ['Inventory', 'Service', 'About Us', 'Contact']
-const logoSrc = '/Intercoastal.jpg'
+const logoSrc = '/logo.jpg'
 
 const inventoryCategories = [
   {
