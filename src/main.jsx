d16a4ff2745679172d29621bd918eb1557f4ac7c
TanyaA318 <tata4625@gmail.com>
2026-03-19 17:26:05 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,3 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Inventory from "./Inventory";
-
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
