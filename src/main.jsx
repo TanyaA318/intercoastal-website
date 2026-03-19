ad83600b1624029a5909c5b6f2f08ddebc5c7513
TanyaA318 <tata4625@gmail.com>
2026-03-19 17:28:53 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import Inventory from "./Inventory";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./styles.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
