1cf587578dfb3aa5d737a56aeee6a86d0821689e
TanyaA318 <tata4625@gmail.com>
2026-03-19 17:30:26 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,1 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import Inventory from "./Inventory";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./styles.css";
 
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/inventory" element={<Inventory />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
