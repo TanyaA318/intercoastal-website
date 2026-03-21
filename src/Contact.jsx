56b743794c4080f051c21ab9457a71704eb3e5ea
TanyaA318 <tata4625@gmail.com>
2026-03-21 14:32:10 -0400

    Update Contact.jsx

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -1,1 +1,9 @@
-
+export default function Contact() {
+  return (
+    <div style={{ padding: "40px" }}>
+      <h1>Contact Us</h1>
+      <p>168 US-158 W Camden, NC</p>
+      <p>Phone: (Add your number here)</p>
+    </div>
+  );
+}
