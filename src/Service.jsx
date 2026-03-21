01c3c29d760ac5e530bfe43d4edfd5ce84b9bbaf
TanyaA318 <tata4625@gmail.com>
2026-03-21 14:31:41 -0400

    Update Service.jsx

--- src/Service.jsx
+++ src/Service.jsx
@@ -1,1 +1,8 @@
-
+export default function Service() {
+  return (
+    <div style={{ padding: "40px" }}>
+      <h1>Service & Repairs</h1>
+      <p>Schedule an appointment or learn about our service offerings.</p>
+    </div>
+  );
+}
