209a7052abb853687a975e8834486b25fbb3c011
TanyaA318 <tata4625@gmail.com>
2026-03-30 14:41:59 -0400

    Update Contact.jsx

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -11,28 +11,7 @@ export default function Contact() {
         </div>
 
         <div className="contact-page contact-page-structured">
-          <div className="contact-socials">
-  <p className="social-text">Prefer to reach out another way?</p>
-
-  <a
-    href="https://facebook.com/"
-    target="_blank"
-    rel="noreferrer"
-    className="social-btn"
-  >
-    Message Us on Facebook
-  </a>
-
-  <a
-    href="https://instagram.com/"
-    target="_blank"
-    rel="noreferrer"
-    className="social-btn social-btn-outline"
-  >
-    Follow Us on Instagram
-  </a>
-</div>
-          <div className="contact-info-panel">
+         <div className="contact-info-panel">
             <div className="contact-info-block contact-info-block-compact">
               <h3>Address</h3>
               <p>168 US-158 W Camden, NC</p>
