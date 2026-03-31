0f682dde67e953d1a995f83c98718a1a040fd460
TanyaA318 <tata4625@gmail.com>
2026-03-31 16:12:07 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,26 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-import Inventory from "./Inventory";
-import Service from "./Service";
-import About from "./About";
-import Contact from "./Contact";
-import Header from "./Header";
 import "./styles.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
-      <Header />
-
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/inventory" element={<Inventory />} />
-        <Route path="/service" element={<Service />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
+      <App />
     </BrowserRouter>
   </React.StrictMode>
 );
