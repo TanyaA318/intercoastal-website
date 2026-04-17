0d49f2c1c908ecc1ed289278d85b5d6d037357ac
TanyaA318 <tata4625@gmail.com>
2026-04-17 14:09:58 -0400

    Update InventoryNew.jsx

--- src/InventoryNew.jsx
+++ src/InventoryNew.jsx
@@ -1,1 +1,45 @@
+import React from "react";
+import Header from "./Header";
 
+const mowers = [
+  {
+    name: "Bad Boy Maverick HD 54”",
+    image: "/mower1.jpg",
+    price: "Call for Price",
+  },
+  {
+    name: "Bad Boy Maverick 60”",
+    image: "/mower1.jpg",
+    price: "Call for Price",
+  },
+  {
+    name: "Bad Boy Residential Series",
+    image: "/mower-hero.jpg",
+    price: "Call for Price",
+  },
+];
+
+export default function InventoryNew() {
+  return (
+    <>
+      <Header />
+
+      <div className="inventory-list-page">
+        <h1>New Equipment</h1>
+
+        <div className="inventory-list-grid">
+          {mowers.map((mower) => (
+            <div className="inventory-list-card" key={mower.name}>
+              <img src={mower.image} alt={mower.name} />
+              <h3>{mower.name}</h3>
+              <p className="price">{mower.price}</p>
+              <a href="/contact" className="inventory-button">
+                Get Info
+              </a>
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
+  );
+}
